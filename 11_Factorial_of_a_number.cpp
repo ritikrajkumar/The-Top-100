a99f54bc66205ccd2822dec5a2a17a24d76090ff
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-11 01:09:22 +0530

    Add files via upload

---
+++ 11_Factorial_of_a_number.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num, fact = 1;
+    cout << "Enter the number: ";
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    cout << "The factorial of " << num << " is: " << fact;
+    return 0;
+}

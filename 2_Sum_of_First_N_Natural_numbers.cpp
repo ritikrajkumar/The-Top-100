b5e62536ee82d9b37b35cb8ce073d5d73bd2ea24
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-11 01:06:51 +0530

    Add files via upload

---
+++ 2_Sum_of_First_N_Natural_numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    cout << "The sum is " << num * (num + 1) / 2;
+
+    return 0;
+}

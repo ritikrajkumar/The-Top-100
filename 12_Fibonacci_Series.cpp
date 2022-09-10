99b8eb5bf23403a9280a95ca16c6d76088dd6761
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-11 01:09:37 +0530

    Add files via upload

---
+++ 12_Fibonacci_Series.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+    else
+    {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
+
+int main()
+{
+    int num, result;
+    cout << "Enter the number: ";
+    cin >> num;
+    cout << "The desired series is: ";
+    for (int i = 0; i < num; i++)
+    {
+        cout << fibo(i) << " ";
+    }
+    return 0;
+}

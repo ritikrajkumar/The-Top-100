3c43133a093f2f172fbf98132ebcfbd655198065
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-11 01:06:33 +0530

    Add files via upload

---
+++ 1_Even_or_Odd_number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << "It is an even number.";
+    else
+        cout << "It is an odd number.";
+
+    return 0;
+}

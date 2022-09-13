583cce935d42c63e0417c2ef9ed378465be550ab
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-13 22:56:09 +0530

    Update 30_Common_elements_from_two_arryas.cpp

--- 30_Common_elements_from_two_arryas.cpp
+++ 30_Common_elements_from_two_arryas.cpp
@@ -6,7 +6,7 @@ int main()
 	int arr2[] = {4, 5, 95, 11, 6, 10};
 	int size1 = sizeof(arr1) / sizeof(arr1[0]);
     int size2 = sizeof(arr2) / sizeof(arr2[0]);
-	int i, j, flag, k = 0;
+	int i, j, k = 0;
 	int result[100];
 	printf("The common elements are: ");
 	for (i = 0; i < size1; i++)

2d9561fc119f514d8a08bf852ea449ed9e6f6856
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-13 21:47:15 +0530

    Add files via upload

---
+++ 15_Ascii_value_of_given_character.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter the character: ";
+    cin >> ch;
+    cout << "The ascii value of given character is " << int(ch) << ".";
+    return 0;
+}

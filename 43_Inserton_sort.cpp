ecd2edb2dbe05bbd9ce4f8d4c5fb2c91bba2ef59
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2022-09-16 22:02:56 +0530

    Add files via upload

---
+++ 43_Inserton_sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void insertionSort(int *arr, int n)
+{
+    int key, j;
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {12, 74, 89, 3, 14, 25, 47, 99};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << "The given array is: ";
+    printArray(arr, size);
+    insertionSort(arr, size);
+    cout << "\nThe sorted array is: ";
+    printArray(arr, size);
+    return 0;
+}
